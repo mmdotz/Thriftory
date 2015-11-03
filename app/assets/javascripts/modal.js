df5f37f27c8006e70e8142148bada78b12c5b869
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-03 14:23:17 -0600

    style log in page

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,3 +2,8 @@
 $(document).ready(function() {
     $('#myModal').modal('show');
 });
+
+//I want these to happen when the button is pressed
+$(document).ready(function(){
+   $('#photoModal').modal();
+ });
