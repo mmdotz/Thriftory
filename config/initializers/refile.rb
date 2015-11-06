9e7c2123ad7a57ac8ac208978209fd193adaef83
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-05 18:18:23 -0600

    change keys to strings in refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,8 +1,8 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: ENV[:AWS_KEY],
-  secret_access_key: ENV[:AWS_KEY_SECRET],
+  access_key_id: ENV["AWS_KEY"],
+  secret_access_key: ENV["AWS_KEY_SECRET"],
   region: "sa-east-1",
   bucket: "dotzenrod-rails-prod",
 }
