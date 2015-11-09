2304259c0847f8f077883dc829660557e0ba98e7
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-09 11:54:46 -0600

    Adjusting Migrations.

--- db/migrate/20151026184847_create_sources.rb
+++ db/migrate/20151026184847_create_sources.rb
@@ -2,7 +2,7 @@ class CreateSources < ActiveRecord::Migration
   def change
     create_table :sources do |t|
       t.string    :name,     default: "unknown"
-      t.datetime  :date,   default: Date.today
+      t.datetime  :date,     default: Date.today
       t.string    :address,  default: "unknown"
       t.string    :type,     null: false
 
--- db/migrate/20151029163336_add_foreign_key_to_sources.rb
+++ db/migrate/20151029163336_add_foreign_key_to_sources.rb
@@ -1,5 +1,6 @@
 class AddForeignKeyToSources < ActiveRecord::Migration
   def change
+    add_column :sources, :user_id, :integer
     add_foreign_key :sources, :users
   end
 end
