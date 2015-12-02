b88f45f006adc30bd58157e84159aa6838665086
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-01 20:12:25 -0600

    corrected pie charts for status & condition by current_user

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,11 +3,11 @@ class StatsController < ApplicationController
     @items = current_user.items
     @inventory = current_user.items.count
 
-
+    #correct
     @moneyin = current_user.finances.sum("buyer_pmt")
     @moneyout = current_user.finances.all.map { |finance| finance.item_total_outlay }.sum
 
-    #this needs be be filtered thru the current_user
+    #this needs be be filtered thru the current_user, showing db totals
     @payment_by_category = Hash[Category.all.map(&:finances).map { |fin_array| fin_array.map(&:buyer_pmt).sum
     }.map.with_index { |amt, index| [Category.find(index + 1).name, amt] }]
 
