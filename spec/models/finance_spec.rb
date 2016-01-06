d8ce7576d65f44365008febb614eee4c7910d810
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-06 08:08:31 -0600

    config rspec for finance model

---
+++ spec/models/finance_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+
+describe Finance do
+
+  describe  '#item_total_outlay' do
+    it 'adds the seller costs associated with item' do
+      # procure_cost + shipping_cost + pre_sale_cost
+
+    end
+  end
+
+end
