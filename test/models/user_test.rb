ec6c44138ec09ad1badba58c47a5b73d1ac74113
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-17 13:25:55 -0600

    writing many damn failing tests, one passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def valid_params
-    { username: "pete", email: "pete@example.com"}
+    {
+      username: "peteR",
+      email: "pete@example.com",
+      password_digest: "password"
+    }
   end
 
   def test_valid
@@ -12,20 +16,31 @@ class UserTest < ActiveSupport::TestCase
     assert(user.valid?, "Can't create! See test_valid")
   end
 
-  def test_invalid_without_email
-    params = valid_params.clone
-    params.delete :email
-    user = User.new(params)
+  def test_invalid_without_email #passing because there is a model validation for password length
+    params = valid_params.clone #given that I valid params
+    params.delete :email #and I alter those params to not include an email
+    user = User.create(params) #and I try to create a user without valid params
 
-    refute(user.valid?, "Can't be valid without email")
+    refute(user.valid?, "Can't be valid without email") #then the user object should not be saved
     assert(user.errors[:email], "Missing error when without email")
   end
 
   def test_username_is_unique
-    user = User.create(valid_params)
-    another_user = user.clone
+    user = User.create(valid_params) #given a user is created with valid params
+    another_user = user.clone #and I create another user by cloning user
 
-    assert(another_user.valid?, "Username must be unique.")
+    assert(another_user.valid?, "Username must be unique.") #then another_user should not be saved
+  end
+
+  def test_downcase_callback
+    user = User.new(valid_params)
+    user.save
+
+    assert_equal("peter", user.username)
+  end
+
+  def test_geocoder_creates_latandlong
+    #address is saved then geocoder adds lat and long to record
   end
 
 end
