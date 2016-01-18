a7849ae0397c2f41d25a3f02463ffa1dc6601cbc
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-18 11:54:22 -0600

    correct item fixtures for current schema

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_item_creation_triggers_creation_of_finance
+  end
+
+  def test_dependencies_are_destroyed_on_item_delete
+  end
+
 end
