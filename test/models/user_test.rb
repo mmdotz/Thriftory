c3a8869cb26bcda14c5d6ec350661aee7c0e126c
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-18 11:27:11 -0600

    user model tests passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,7 +19,7 @@ class UserTest < ActiveSupport::TestCase
 
   def test_invalid_without_email        #passing because there is a no model validation for email
     params = valid_params.clone         #given that I enter valid params
-    params.delete :email                #and I alter those params to not include an email
+    params.delete :email                #when I alter those params to not include an email
     user = User.create(params)          #and I try to create a user without valid params
 
     refute(user.valid?, "Can't be valid without email") #then the user object should not be saved
@@ -28,7 +28,7 @@ class UserTest < ActiveSupport::TestCase
 
   def test_username_is_unique           #passing
     user = User.create(valid_params)    #given a user is created with valid params
-    another_user = user.clone           #and I create another user by cloning user
+    another_user = user.clone           #when I create another user by cloning user
 
     assert(another_user.valid?, "Username must be unique.")
                                         #then another_user should not be saved
@@ -36,17 +36,17 @@ class UserTest < ActiveSupport::TestCase
 
   def test_downcase_callback            #passing
     user = User.new(valid_params)       #given a user enters uppercase anywhere in username
-    user.save!
+    user.save!                          #when it is saved
 
-    assert_equal("peter", user.username)#it will be saved as downcase
+    assert_equal("peter", user.username)#then it will be saved as downcase
   end
 
   def test_geocoder_creates_latandlong_values
     user = User.new(valid_params)       #given a user is instantiated with an address
-    user.save!                          #and it is saved to the db
-
-    assert_difference((user.latitude).size, 1)
-                                        #then the lat and long attributes change from nil to 1
+    user.save!                          #when it is saved
+    
+    assert(user.latitude)               #then the lat and long attributes
+    assert(user.longitude)              #are populated
   end
 
 end
