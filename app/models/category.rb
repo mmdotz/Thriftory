151954ed6cbba558e35fd89f755a9f74920bb0a6
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-02-04 20:53:38 -0600

    move bloated stats controller to new model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,10 @@ class Category < ActiveRecord::Base
   has_many :subcat2s, through:  :subcat1s
   has_many :items, through: :subcat2s
   has_many :finances, through: :items
+  has_many :users, through: :items
 
   accepts_nested_attributes_for :subcat1s
   validates_associated          :subcat1s
-  validates :name, presence: true
+  validates                     :name, presence: true
 
 end
