62d18c61967cf66cd80349d05ca72ccafac8346e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-02-05 22:23:59 -0600

    move bloat from stats controller to model, update stats index

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,17 +1,10 @@
 class StatsController < ApplicationController
   def index
     @items = current_user.items
-    @inventory = current_user.items.count
-    @total_week_items = @items.where("items.created_at > ?", Time.now-7.days).count
-
-    #correct
-    @moneyin = current_user.finances.sum("buyer_pmt")
-    @moneyout = current_user.finances.all.map { |finance| finance.item_total_outlay }.sum
 
     #this needs be be filtered thru the current_user, showing db totals
     @payment_by_category = Hash[Category.all.map(&:finances).map { |fin_array| fin_array.map(&:buyer_pmt).sum
     }.map.with_index { |amt, index| [Category.find(index + 1).name, amt] }]
-
 
     @furniture_expense = Category.find(1).finances.map{ |f| f.item_total_outlay }.sum
     @clothing_expense = Category.find(2).finances.map{ |f| f.item_total_outlay }.sum
--- app/models/stats.rb
+++ app/models/stats.rb
@@ -1,40 +1,14 @@
 class Stats
 
-  # @items = current_user.items
-  def c_u_items
-    current_user.items
+  def self.total_week_items(user)
+    user.items.where("items.created_at > ?", Time.now-7.days).count
   end
 
-  def c_u_finances
-    current_user.finances
+  def self.moneyin(user)
+    user.finances.sum("buyer_pmt")
   end
 
-  # @inventory = current_user.items.count
-  def c_u_inventory
-    c_u_items.count
+  def self.moneyout(user)
+    user.finances.all.map { |finance| finance.item_total_outlay }.sum
   end
-
-  # @total_week_items = @items.where("items.created_at > ?", Time.now-7.days).count
-  def total_week_items
-    c_u_items.where("items.created_at > ?", Time.now-7.days).count
-  end
-
-  def moneyin
-    c_u_finances.sum("buyer_pmt")
-  end
-
-  def moneyout
-    c_u_finances.all.map { |finance| finance.item_total_outlay }.sum
-  end
-
-  #this needs be be filtered thru the current_user, showing db totals
-  @payment_by_category = Hash[Category.all.map(&:finances).map { |fin_array| fin_array.map(&:buyer_pmt).sum
-  }.map.with_index { |amt, index| [Category.find(index + 1).name, amt] }]
-
-
-  @furniture_expense = Category.find(1).finances.map{ |f| f.item_total_outlay }.sum
-  @clothing_expense = Category.find(2).finances.map{ |f| f.item_total_outlay }.sum
-  @household_expense = Category.find(3).finances.map{ |f| f.item_total_outlay }.sum
-  @other_expense = Category.find(4).finances.map{ |f| f.item_total_outlay }.sum
-
 end
