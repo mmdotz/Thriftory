f8aecb8015993018fb02b9e34bff916bb03a0369
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-02-09 22:07:17 -0600

    trying many things none of which work

--- test/models/finance_test.rb
+++ test/models/finance_test.rb
@@ -1,32 +1,34 @@
 require 'test_helper'
 
 class FinanceTest < ActiveSupport::TestCase
+  i_suck_and_my_tests_are_order_dependent!
 
-  def finance
-    finance(:finance_one)
+  def setup
+    @finance = finance(:finance_one)
+    @item_total_outlay = @finance.procure_cost + @finance.shipping_cost + @finance.pre_sale_cost
   end
 
-  def test_valid
-    assert finance.valid?
+  def test_validity #wtf isn't this running??
+    assert(@finance.valid?)
   end
 
-  def test_invalid_without_user
-    finance.user = nil
-    assert finance.invalid?
+  def test_invalid_without_item
+    @finance.item = nil
+    assert(@finance.invalid?)
   end
 
   def test_item_total_outlay
-    assert_equal(3, finance.procure_cost + finance.shipping_cost + finance.pre_sale_cost)
+    assert_equal(3, @item_total_outlay )
   end
 
 
   def test_sum_total_outlay
     skip
-    #assert_equal(,)
-  end
+    #this requires all current_user's finances
+   end
 
-  def test_item_profit? #single item buyer_pmt, item_total_outlay
-    skip
+  def test_item_profit? #current fixtures make item non profitable
+    assert(@finance.buyer_pmt > @item_total_outlay)
     # check self.buyer_pmt > self.item_total_outlay
     # given i have an item with seller costs (buyer costs may be 0)
     # when I add up the seller costs
