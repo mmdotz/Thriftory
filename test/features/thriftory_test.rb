3634c8b418a78070d0c43808b884fc68a5917ecb
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-02-11 21:17:15 -0600

    test writing is fun

--- test/features/thriftory_test.rb
+++ test/features/thriftory_test.rb
@@ -1,14 +1,48 @@
 require "test_helper"
 
-feature "Thriftory" do
-  scenario "home page appears" do
-    visit home_path
+#doesn't this need to be in a Class inheriting from Capybara?
+test "login" do
+
+    @user = User.new(:email => 'jayd@example.com')
+    @user.password = 'password'
+    @user.save!
+
+    visit root_path
+    click_link 'Login'
+
+    fill_in "email", with: 'jayd@example.com'
+    fill_in "password", with: 'password'
+    click_button "Sign In"
+  end
+
+
+feature "Thriftory" do #passing
+  scenario "login page appears" do
+    visit home_path #cannot access until logged in
     page.must_have_css(".logo")
-    page.wont_have_content "Goobye All!"
+    page.wont_have_content("thrill") #home page content
   end
 end
 
-describe "SourcesController" do #failing
+#set up access to this page by logging in first
+feature "Thriftory" do
+  scenario "displays a list of sources" do
+    visit sources_path
+    page.must_have_css(".sources")
+    within(".sources") do
+      Source.find_each do |source|
+        selector = ".source-#{ source.id }"
+        page.must_have_css(selector)
+        within(selector) do
+          page.must_have_content(source.name)
+          page.must_have_content(source.address)
+        end
+      end
+    end
+  end
+end
+
+describe "SourcesController" do 
   describe "GET :index" do
     before do
       get :index
