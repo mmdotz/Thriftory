f980e12bf8ba24204b3fa043d8dbc565defdddf6
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-02-13 12:49:38 -0600

    more fabulous failing tests, second login test attempt

--- test/features/thriftory_test.rb
+++ test/features/thriftory_test.rb
@@ -1,23 +1,41 @@
 require "test_helper"
 
-#doesn't this need to be in a Class inheriting from Capybara?
-test "login" do
+# doesn't this need to be in a Class inheriting from Capybara?
+test "login" do #fails: ArgumentError: wrong number of arguments (1 for 2)
 
     @user = User.new(:email => 'jayd@example.com')
     @user.password = 'password'
+    @user.username = 'jaysond'
+    @user.address = '339 Maple Street, New Bedford, MA 02740'
     @user.save!
 
     visit root_path
-    click_link 'Login'
 
     fill_in "email", with: 'jayd@example.com'
     fill_in "password", with: 'password'
     click_button "Sign In"
   end
 
+feature 'Create an Account' do
+  scenario 'user enters valid input' do
+    visit '/users/new'
+
+    within '#user_registration' do #fails, cannot find field username
+      fill_in 'username', with: 'jayson'
+      fill_in 'password', with: 'password'
+      fill_in 'address',  with: '337 Maple Street, New Bedford, MA 02740'
+      fill_in 'email',    with: 'jd@example.com'
+      click_button 'Create User' #or should this be link?
+    end
+
+    page.must_have_content 'Please sign in!'
+    page.current_path.must_equal '/login'
+  end
+end
+
 
 feature "Thriftory" do #passing
-  scenario "login page appears" do
+  scenario "redirect to login page if not signed in" do
     visit home_path #cannot access until logged in
     page.must_have_css(".logo")
     page.wont_have_content("thrill") #home page content
@@ -42,7 +60,7 @@ feature "Thriftory" do
   end
 end
 
-describe "SourcesController" do 
+describe "SourcesController" do
   describe "GET :index" do
     before do
       get :index
