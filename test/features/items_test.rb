e82b5c607b9b55cd1e52b9b9822f2d03122ecf46
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-19 21:29:04 -0500

    rewrote feature tests, still failing on login, not creating inventory

--- test/features/items_test.rb
+++ test/features/items_test.rb
@@ -1,10 +1,17 @@
 require "test_helper"
 
-feature "view items" do
+feature "create_inventory_for_test_method_builds_an_item" do #testing a test??
+  scenario "current_user_for_test's inventory is created" do
+    @item.wont_be_nil
+  end
+end
+
+feature "view items" do # failing on log in
   scenario "current_user_for_test's inventory appears", js: true do
-    login_user_for_test #not logging in
+    login_user_for_test # in test_helper
+    sleep 2
     create_inventory_for_test
     visit items_path
-    page.must_have_content("book") #current_user_for_test's only item
+    page.must_have_content("vintage") #current_user_for_test's only item
   end
 end
