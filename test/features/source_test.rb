253aea68bfe7da073b1634b263452ed8eb4fc417
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-22 19:17:39 -0500

    removed selenium code, passing

--- test/features/source_test.rb
+++ test/features/source_test.rb
@@ -1,16 +1,16 @@
 require "test_helper"
 
-feature "Source" do # passing
+feature "Source" do # passing only without selenium below
   scenario "displays newly created source" do
     login_user_for_test
     source = Source.create(
       name: "Jim's Treasures",
       address: "129 Angell Street, Providence. RI 02912",
       type_of: "store",
-      user_id: current_user_for_test.id
+      user_id: current_user_for_test.id # couldn't this be @current_user ?
       )
     visit sources_path
-    current_user_for_test.sources.find_each do |source|
+    current_user_for_test.sources.find_each do |source| # current_user?
       page.must_have_content("Treasures")
       page.must_have_content(source.address)
     end
